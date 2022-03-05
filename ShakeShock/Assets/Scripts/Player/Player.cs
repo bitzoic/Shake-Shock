65736032f6dfbe9fc3813a615fb5cd8b59d8ed35
bitzoic <bitzoic.eth@gmail.com>
2022-03-05 14:20:35 -0500

    Game basics

---
+++ ShakeShock/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,29 @@
+// --------------------------------------------------------------
+// Shake Shock - Player                                 3/05/2022
+// Author(s): Cameron Carstens
+// Contact: bitzoic.eth@gmail.com
+// --------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    #region Monobehaviors
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Load all NFT data here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
