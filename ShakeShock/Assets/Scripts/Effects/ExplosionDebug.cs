079eb3d73b693adff8146e9a83c6ebe8c670d70e
bitzoic <bitzoic.eth@gmail.com>
2022-03-06 12:56:05 -0500

    Basic explosion

---
+++ ShakeShock/Assets/Scripts/Effects/ExplosionDebug.cs
@@ -0,0 +1,46 @@
+// --------------------------------------------------------------
+// Shake Shock - ExplosionDebug                         3/06/2022
+// Author(s): Cameron Carstens
+// Contact: bitzoic.eth@gmail.com
+// --------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionDebug : MonoBehaviour
+{
+
+    #region Inspector Fields
+
+    [Header("Dependencies")]
+    [SerializeField]
+    private GameObject explosionPrefab;
+    [SerializeField]
+    private GameObject debugGameObject;
+    [SerializeField]
+    private Camera mainCamera;
+
+    [Header("Settings")]
+    [SerializeField]
+    private bool enabled;
+
+    #endregion
+
+    private void Start()
+    {
+        if (!enabled)
+        {
+            Destroy(debugGameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(explosionPrefab, mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1)), Quaternion.identity);
+        }
+    }
+}
